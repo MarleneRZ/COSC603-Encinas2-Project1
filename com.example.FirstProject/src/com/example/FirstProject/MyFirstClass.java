3487b112a27dbc5fd9e6ce331acd17d663ce7e20
MarleneRZ <lynn2301@verizon.net>
2016-02-19 19:11:51 -0500

    First Test
    test

---
+++ com.example.FirstProject/src/com/example/FirstProject/MyFirstClass.java
@@ -0,0 +1,24 @@
+/*
+ * 
+ */
+package com.example.FirstProject;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MyFirstClass.
+ */
+public class MyFirstClass {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Eclipse!");
+
+	}
+
+}
